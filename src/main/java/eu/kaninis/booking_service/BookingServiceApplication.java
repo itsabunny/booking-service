18ec693817a9105a22ca7543c80f61550115a727
Ninis Blomerus <itsabunny@pm.me>
2025-11-18 11:01:16 +0100

    fix: correct indentation in main method

--- src/main/java/eu/kaninis/booking_service/BookingServiceApplication.java
+++ src/main/java/eu/kaninis/booking_service/BookingServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingServiceApplication.class, args);
+        SpringApplication.run(BookingServiceApplication.class, args);
 	}
 
 }
